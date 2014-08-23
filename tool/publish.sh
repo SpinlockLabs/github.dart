1ec9e4266780fa53f149781b76e3a777d0af4f70
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-23 18:32:39 -0400

    Publish Script: Allow keeping version.

--- tool/publish.sh
+++ tool/publish.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-# Publishes an irc.dart release
-./tool/build.dart publish
+# Publishes a GitHub.dart release
+if [ ${1} != "--keep-version" ]
+then
+  ./tool/build.dart publish
+fi
+
 VERSION=`grep 'version:' pubspec.yaml | sed 's/version: //'`
 echo Releasing ${VERSION}
 git add .
