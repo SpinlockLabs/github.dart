fad5644a567725f8365bcd71f4974a7e940bdd90
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-22 22:26:43 -0400

    Push Demos to on the Update Demos Script

--- tool/update-demos.sh
+++ tool/update-demos.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ -z ${1} ]
 then
   echo "Usage: tool/update-demos.sh path/to/demos"
@@ -7,8 +9,12 @@ then
 fi
 
 rm -rf build
-
 rm -rf ${1}
 
 pub build example --mode=debug
 cp -R build/web ${1}
+
+cd ${1}
+git add .
+git commit -m "Updating Demos"
+git push
