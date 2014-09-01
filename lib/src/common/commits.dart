1cb5f0ce8401d8717122d6ca2ecab515d1d7fdae
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-31 20:31:33 -0400

    Support for getting changed files in a commit.

--- lib/src/common/commits.dart
+++ lib/src/common/commits.dart
@@ -11,26 +11,26 @@ class Commit {
    */
   @ApiName("html_url")
   String url;
-  
+
   /**
    * Commit SHA
    */
   String sha;
-  
+
   String treeSha;
-  
+
 
   /**
    * Commit Message
    */
   @ApiName("commit/message")
   String message;
-  
+
   /**
    * Commit Author
    */
   User author;
-  
+
   /**
    * Commit Commiter
    */
@@ -59,7 +59,7 @@ class Commit {
    */
   @ApiName("commit/author/date")
   DateTime authoredAt;
-  
+
   /**
    * Time this commit was committed at
    */
@@ -71,17 +71,19 @@ class Commit {
    */
   @ApiName("commit/commiter/email")
   String committerEmail;
-  
+
   /**
    * Author Email
    */
   @ApiName("commit/author/email")
   String authorEmail;
 
+  List<ChangedFile> files;
+
   Commit(this.github);
 
   Map<String, dynamic> json;
-  
+
   static Commit fromJSON(GitHub github, input) {
     var commit = new Commit(github)
         ..url = input['html_url']
@@ -95,7 +97,7 @@ class Commit {
         ..treeSha = input['tree']['sha'];
 
     commit.json = input;
-    
+
     if (input['stats'] != null) {
       commit
           ..additionsCount = input['stats']['additions']
@@ -103,6 +105,48 @@ class Commit {
           ..commentsCount = input['commit']['comments_count'];
     }
 
+    if (input['files'] != null) {
+      commit.files = input['files'].map((it) => ChangedFile.fromJSON(github, it)).toList();
+    }
+
     return commit;
+  }
+}
+
+class ChangedFile {
+  final GitHub github;
+
+  @ApiName("filename")
+  String name;
+
+  int additions;
+  int deletions;
+  int changes;
+  String status;
+
+  @ApiName("raw_url")
+  String rawUrl;
+
+  @ApiName("blob_url")
+  String blobUrl;
+
+  String patch;
+
+  Map<String, dynamic> json;
+
+  ChangedFile(this.github);
+
+  static ChangedFile fromJSON(GitHub github, input) {
+    if (input == null) return null;
+    return new ChangedFile(github)
+        ..name = input['filename']
+        ..additions = input['additions']
+        ..deletions = input['deletions']
+        ..changes = input['changes']
+        ..status = input['status']
+        ..rawUrl = input['raw_url']
+        ..blobUrl = input['blob_url']
+        ..patch = input['patch']
+        ..json = input;
   }
 }
