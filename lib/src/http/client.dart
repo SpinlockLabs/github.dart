bc049ff536cf3a6e527ab90bb89ba1f1a95b0740
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-05 19:07:08 -0400

    Add http.Client get method.

--- lib/src/http/client.dart
+++ lib/src/http/client.dart
@@ -2,4 +2,8 @@ part of github.http;
 
 abstract class Client {
   Future<Response> request(Request request);
+  
+  Future<Response> get(String url, {Map<String, String> headers}) {
+    return request(new Request(url, method: "GET", headers: headers));
+  }
 }
