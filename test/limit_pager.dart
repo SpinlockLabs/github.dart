925a48628dcbb061ac33a72824ebf49d993e48dd
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-06 13:45:20 -0400

    Added an experimentation test for the upcoming pagination solver.

---
+++ test/limit_pager.dart
@@ -0,0 +1,43 @@
+import "dart:math";
+
+void main() {
+  print(solve(500));
+  print(solve(20));
+  print(solve(519));
+  print(solve(201));
+}
+
+const int MAX_PER_PAGE = 100;
+const int ACCURACY_RANGE = 5;
+
+/**
+ * Solves the most efficient way to fetch the number of objects [limit] with the least requests.
+ */
+PaginationInformation solve(int limit) {
+  if (limit < 0) {
+    throw new RangeError("limit cannot be less than zero (was ${limit})");
+  }
+  
+  if (limit < MAX_PER_PAGE) {
+    return new PaginationInformation(limit, 1, limit);    
+  }
+  
+  if ((limit % MAX_PER_PAGE) == 0) {
+    return new PaginationInformation(limit, limit ~/ MAX_PER_PAGE, MAX_PER_PAGE);
+  }
+  
+  int itemsPerPage = 100;
+  int pages = (limit / itemsPerPage).ceil();
+  
+  return new PaginationInformation(limit, pages, itemsPerPage);
+}
+
+class PaginationInformation {
+  final int limit;
+  final int itemsPerPage;
+  final int pages;
+  
+  PaginationInformation(this.limit, this.pages, this.itemsPerPage);
+  
+  String toString() => "limit: ${limit}, pages: ${pages}, per page: ${itemsPerPage}";
+}
