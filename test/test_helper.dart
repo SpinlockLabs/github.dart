5b3b668a6fc89339992dc2cc51b585d87abfd963
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-05 23:47:47 -0400

    Fixes for Tests

--- test/test_helper.dart
+++ test/test_helper.dart
@@ -2,13 +2,7 @@ library test_helper;
 
 import 'package:unittest/unittest.dart';
 
-import 'package:github/src/common/util.dart';
-
-void main() {
-  test("https://api.github.com/repos/DirectMyFile/irc.dart slug is correct", () {
-    expectSlug(slugFromAPIUrl("https://api.github.com/repos/DirectMyFile/irc.dart"), "DirectMyFile", "irc.dart");
-  });
-}
+import 'package:github/server.dart';
 
 void expectSlug(RepositorySlug slug, String user, String repo) {
   expect(slug.fullName, equals("${user}/${repo}"));
--- test/utils_test.dart
+++ test/utils_test.dart
@@ -3,9 +3,10 @@ library utils_tests;
 import 'package:unittest/unittest.dart';
 
 import 'package:github/src/common/util.dart';
+import 'test_helper.dart';
 
 void main() {
   test("https://api.github.com/repos/DirectMyFile/irc.dart slug is correct", () {
-    
-    });
+    expectSlug(slugFromAPIUrl("https://api.github.com/repos/DirectMyFile/irc.dart"), "DirectMyFile", "irc.dart");
+  });
 }
