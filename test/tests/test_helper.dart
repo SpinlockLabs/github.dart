9693d7cdb3acdad974d2d1d9b373f39640cf4740
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-18 10:58:33 -0400

    Remove old test helper

--- test/tests/test_helper.dart
+++
@@ -1,9 +0,0 @@
-library test_helper;
-
-import 'package:unittest/unittest.dart';
-
-import 'package:github/server.dart';
-
-void expectSlug(RepositorySlug slug, String user, String repo) {
-  expect(slug.fullName, equals("${user}/${repo}"));
-}
