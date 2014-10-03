87d931dcf72d55faad7fcff2a8b2164db88be857
Kenneth Endfinger <kaendfinger@gmail.com>
2014-10-02 20:32:08 -0400

    Add JUnit Test Configuration

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -2,11 +2,14 @@ library github.test.all_tests;
 
 import 'package:unittest/unittest.dart';
 
+import 'helper.dart';
+
 import 'util_test.dart' as util_test;
 import 'git_test.dart' as git_test;
 
 /// Runs all unit tests.
 void main() {
+  initUnitTests();
   group('[util_test]', util_test.main);
   group('[git_test]', git_test.main);
 }
--- test/helper.dart
+++ test/helper.dart
@@ -7,6 +7,8 @@ import 'package:github/server.dart';
 
 import 'package:github/http.dart' as http;
 
+import 'package:junitconfiguration/junitconfiguration.dart';
+
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
@@ -15,3 +17,15 @@ part 'helper/http.dart';
 part 'helper/mock.dart';
 part 'helper/expect.dart';
 part 'helper/assets.dart';
+
+void initUnitTests() {
+  var dir = new Directory("build");
+
+  if (!dir.existsSync()) dir.createSync();
+
+  var file = new File("${dir.path}/tests.xml");
+
+  if (file.existsSync()) file.deleteSync();
+
+  JUnitConfiguration.install(output: file.openWrite());
+}
