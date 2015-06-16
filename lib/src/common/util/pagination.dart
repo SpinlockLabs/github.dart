57211b9755aac3bf126407bffdb5299d5ecaf3f9
Kevin Moore <kevmoo@google.com>
2015-06-16 13:37:48 -0700

    Remove fetch from PaginationService

--- lib/src/common/util/pagination.dart
+++ lib/src/common/util/pagination.dart
@@ -6,54 +6,6 @@ class PaginationHelper<T> {
 
   PaginationHelper(this.github);
 
-  Future<List<http.Response>> fetch(String method, String path, {int pages,
-      Map<String, String> headers, Map<String, dynamic> params, String body,
-      int statusCode: 200}) {
-    var completer = new Completer();
-    var responses = [];
-    if (headers == null) headers = {};
-    Future<http.Response> actualFetch(String realPath) {
-      return github.request(method, realPath,
-          headers: headers, params: params, body: body, statusCode: statusCode);
-    }
-
-    void done() => completer.complete(responses);
-
-    var count = 0;
-
-    handleResponse(http.Response response) {
-      count++;
-      responses.add(response);
-
-      if (!response.headers.containsKey("link")) {
-        done();
-        return;
-      }
-
-      var info = parseLinkHeader(response.headers['link']);
-
-      if (!info.containsKey("next")) {
-        done();
-        return;
-      }
-
-      if (pages != null && count == pages) {
-        done();
-        return;
-      }
-
-      var nextUrl = info['next'];
-
-      actualFetch(nextUrl).then(handleResponse);
-    }
-
-    actualFetch(path).then(handleResponse).catchError((e, s) {
-      completer.completeError(e, s);
-    });
-
-    return completer.future;
-  }
-
   Stream<http.Response> fetchStreamed(String method, String path, {int pages,
       bool reverse: false, int start, Map<String, String> headers,
       Map<String, dynamic> params, String body, int statusCode: 200}) {
