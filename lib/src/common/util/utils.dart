a0269601c703cbc3affa3aaa2651a4cbb63beb35
Kevin Moore <kevmoo@google.com>
2015-06-16 12:57:08 -0700

    TODOs

--- lib/src/common/util/utils.dart
+++ lib/src/common/util/utils.dart
@@ -84,6 +84,7 @@ void putValue(String name, dynamic value, Map<String, dynamic> map) {
   }
 }
 
+//TODO(kevmoo): use regex here.
 Map<String, String> parseLinkHeader(String input) {
   var out = {};
   var parts = input.split(", ");
@@ -139,6 +140,7 @@ int parseFancyNumber(String input) {
   return value;
 }
 
+//TODO(kevmoo) Hide this. Should not be visible.
 Map<String, dynamic> createNonNullMap(Map<String, dynamic> input) {
   var map = {};
   for (var key in input.keys) {
