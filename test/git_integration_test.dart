b736ce65e7c94292b2612cf0c40777d9d89a5ce6
Kevin Moore <kevmoo@google.com>
2015-11-19 16:22:29 -0800

    use async in integration tests

--- test/git_integration_test.dart
+++ test/git_integration_test.dart
@@ -34,30 +34,26 @@ main() {
     firstCommitTreeSha = branch.commit.tree.sha;
   });
 
-  test('create and get a new blob', () {
+  test('create and get a new blob', () async {
     CreateGitBlob newBlob = new CreateGitBlob('bbb', 'utf-8');
 
-    var createdBlobSha;
-
     // createBlob()
-    return github.git.createBlob(slug, newBlob).then((createdBlob) {
-      createdBlobSha = createdBlob.sha;
+    var createdBlob = await github.git.createBlob(slug, newBlob);
+    var createdBlobSha = createdBlob.sha;
 
-      // getBlob()
-      return github.git.getBlob(slug, createdBlobSha);
-    }).then((fetchedBlob) {
-      expect(base64ToUtf8(fetchedBlob.content), equals('bbb'));
-      expect(fetchedBlob.encoding, equals('base64'));
-      expect(
-          fetchedBlob.url,
-          equals(
-              'https://api.github.com/repos/${slug.fullName}/git/blobs/${createdBlobSha}'));
-      expect(fetchedBlob.sha, equals(createdBlobSha));
-      expect(fetchedBlob.size, equals(3));
-    });
+    var fetchedBlob = await github.git.getBlob(slug, createdBlobSha);
+
+    expect(base64ToUtf8(fetchedBlob.content), equals('bbb'));
+    expect(fetchedBlob.encoding, equals('base64'));
+    expect(
+        fetchedBlob.url,
+        equals(
+            'https://api.github.com/repos/${slug.fullName}/git/blobs/${createdBlobSha}'));
+    expect(fetchedBlob.sha, equals(createdBlobSha));
+    expect(fetchedBlob.size, equals(3));
   });
 
-  test('create and get a new tree', () {
+  test('create and get a new tree', () async {
     var entry1 = new CreateGitTreeEntry('README.md', '100644', 'blob',
         content: 'This is a repository for integration tests.');
     var entry2 = new CreateGitTreeEntry('subdir/asdf.txt', '100644', 'blob',
@@ -67,78 +63,68 @@ main() {
       ..baseTree = firstCommitTreeSha;
 
     // createTree()
-    return github.git.createTree(slug, newTree).then((createdTree) {
-      createdTreeSha = createdTree.sha;
+    var createdTree = await github.git.createTree(slug, newTree);
+    createdTreeSha = createdTree.sha;
 
-      // getTree()
-      return github.git.getTree(slug, createdTreeSha);
-    }).then((fetchedTree) {
-      expect(fetchedTree.sha, equals(createdTreeSha));
-      expect(fetchedTree.entries.length, equals(2));
-    });
+    // getTree()
+    var fetchedTree = await github.git.getTree(slug, createdTreeSha);
+
+    expect(fetchedTree.sha, equals(createdTreeSha));
+    expect(fetchedTree.entries.length, equals(2));
   });
 
-  test('create and get a new commit', () {
+  test('create and get a new commit', () async {
     var newCommit = new CreateGitCommit('My test commit', createdTreeSha)
       ..parents = [firstCommitSha];
 
     // createCommit()
-    return github.git.createCommit(slug, newCommit).then((createdCommit) {
-      createdCommitSha = createdCommit.sha;
+    var createdCommit = await github.git.createCommit(slug, newCommit);
+    createdCommitSha = createdCommit.sha;
 
-      // getCommit()
-      return github.git.getCommit(slug, createdCommitSha);
-    }).then((fetchedCommit) {
-      expect(fetchedCommit.sha, equals(createdCommitSha));
-      expect(fetchedCommit.message, equals('My test commit'));
-      expect(fetchedCommit.tree.sha, equals(createdTreeSha));
-      expect(fetchedCommit.parents.first.sha, equals(firstCommitSha));
-    });
+    // getCommit()
+    var fetchedCommit = await github.git.getCommit(slug, createdCommitSha);
+    expect(fetchedCommit.sha, equals(createdCommitSha));
+    expect(fetchedCommit.message, equals('My test commit'));
+    expect(fetchedCommit.tree.sha, equals(createdTreeSha));
+    expect(fetchedCommit.parents.first.sha, equals(firstCommitSha));
   });
 
   test('update heads/master reference to new commit', () {
     return github.git.editReference(slug, 'heads/master', createdCommitSha);
   });
 
-  test('create and get a new reference (branch)', () {
+  test('create and get a new reference (branch)', () async {
     var branchName = _randomGitName();
 
-    return github.git
-        .createReference(slug, 'refs/heads/$branchName', createdCommitSha)
-        .then((createdRef) {
-      return github.git.getReference(slug, 'heads/$branchName');
-    }).then((fetchedRef) {
-      expect(fetchedRef.ref, equals('refs/heads/$branchName'));
-      expect(fetchedRef.object.type, equals('commit'));
-      expect(fetchedRef.object.sha, equals(createdCommitSha));
-    });
+    var createdRef = await github.git
+        .createReference(slug, 'refs/heads/$branchName', createdCommitSha);
+
+    var fetchedRef = await github.git.getReference(slug, 'heads/$branchName');
+    expect(fetchedRef.ref, equals('refs/heads/$branchName'));
+    expect(fetchedRef.object.type, equals('commit'));
+    expect(fetchedRef.object.sha, equals(createdCommitSha));
   });
 
-  test('create and get a new tag', () {
+  test('create and get a new tag', () async {
     var tagName = 'v${_randomGitName()}';
-
-    String createdTagSha;
 
     var newTag = new CreateGitTag(tagName, 'Version 0.0.1', createdCommitSha,
         'commit', new GitCommitUser('aName', 'aEmail', new DateTime.now()));
 
     // createTag()
-    return github.git.createTag(slug, newTag).then((createdTag) {
-      createdTagSha = createdTag.sha;
+    var createdTag = await github.git.createTag(slug, newTag);
+    var createdTagSha = createdTag.sha;
 
-      // getTag()
-      return github.git.getTag(slug, createdTagSha);
-    }).then((fetchedTag) {
-      expect(fetchedTag.tag, equals(tagName));
-      expect(fetchedTag.sha, equals(createdTagSha));
-      expect(fetchedTag.message, equals('Version 0.0.1'));
-      expect(fetchedTag.tagger.name, equals('aName'));
-      expect(fetchedTag.object.sha, equals(createdCommitSha));
-    }).then((_) {
-      // Create a reference for the tag.
-      return github.git
-          .createReference(slug, 'refs/tags/$tagName', createdTagSha);
-    });
+    // getTag()
+    var fetchedTag = await github.git.getTag(slug, createdTagSha);
+    expect(fetchedTag.tag, equals(tagName));
+    expect(fetchedTag.sha, equals(createdTagSha));
+    expect(fetchedTag.message, equals('Version 0.0.1'));
+    expect(fetchedTag.tagger.name, equals('aName'));
+    expect(fetchedTag.object.sha, equals(createdCommitSha));
+
+    // Create a reference for the tag.
+    await github.git.createReference(slug, 'refs/tags/$tagName', createdTagSha);
   });
 }
 
