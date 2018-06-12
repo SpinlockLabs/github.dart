38f3b5f07f8335ca236f49ed618069fd1693b0ec
Kevin Moore <kevmoo@google.com>
2018-06-12 15:43:00 -0700

    cleanup: dartfmt example directory

--- example/octocat.dart
+++ example/octocat.dart
@@ -21,11 +21,9 @@ void loadCat() {
     var index = random.nextInt(cats.length);
     var cat = cats[index];
     print("Selected Octocat at ${index} (${cat.name})");
-    $octocat.appendHtml(
-        """
+    $octocat.appendHtml("""
     <h2>${cat.name}</h2>
     <a class="center" href="${cat.url}"><img src="${cat.image}"></img></a>
-    """,
-        treeSanitizer: NodeTreeSanitizer.trusted);
+    """, treeSanitizer: NodeTreeSanitizer.trusted);
   });
 }
