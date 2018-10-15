85830f29e230cbed27e7e73857e8e7c8f3611657
Kevin Moore <kevmoo@google.com>
2018-10-15 10:39:00 -0700

    Support latest pkg:http version

--- lib/browser.dart
+++ lib/browser.dart
@@ -14,6 +14,9 @@ export "src/common.dart";
 /// Creates a GitHub Client
 GitHub createGitHubClient(
     {Authentication auth, String endpoint: "https://api.github.com"}) {
+  // NOTE: This library is not needed if `pkg:http` is updated to support
+  // pkg:http ^0.12.0. Make sure to update the dependency if/when you remove
+  // browser.dart
   return new GitHub(
       auth: auth, client: new BrowserClient(), endpoint: endpoint);
 }
