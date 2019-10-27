6ba8d4fa3c4c876c0fb980b05422a9051ae95c51
Rob Becker <rob.becker@workiva.com>
2019-10-27 13:12:21 -0600

    Moving unified entry point to the dev branch for 6.0 release

--- lib/github.dart
+++
@@ -1,8 +0,0 @@
-/// The code inspired by the http package from
-/// https://github.com/dart-lang/http/blob/9a17157e6a71972f929a95c6b2b36992e5e02c6d/lib/src/client.dart#L11-L16
-
-export 'package:github/src/github_stub.dart'
-    if (dart.library.html) 'package:github/browser.dart'
-    if (dart.library.io) 'package:github/server.dart';
-
-export 'package:github/src/common.dart';
