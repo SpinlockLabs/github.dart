4a54ee30e24b4adc11dad87b16f4ddc0fe676f3c
Rob Becker <rob.becker@workiva.com>
2020-01-31 08:59:05 -0700

    Add some doc comments

--- lib/src/common/issues_service.dart
+++ lib/src/common/issues_service.dart
@@ -130,6 +130,13 @@ class IssuesService extends Service {
     );
   }
 
+  /// Gets a stream of [Reaction]s for an issue. 
+  /// The optional content param let's you filter the request for only reactions
+  /// of that type. 
+  /// WARNING: ReactionType.plusOne and ReactionType.minusOne currently do not
+  /// work and will throw an exception is used. All others without + or - signs
+  /// work fine to filter.
+  /// 
   /// This API is currently in preview. It may break.
   ///
   /// See https://developer.github.com/v3/reactions/
