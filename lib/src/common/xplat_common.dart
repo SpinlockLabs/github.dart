3a22708adaad5a5a28985c2fc0afc3501202736b
Rob Becker <rob.becker@workiva.com>
2020-04-21 08:03:34 -0600

    prep 6.2.1 and dart format (comment whitespace :facepalm:)

--- lib/src/common/xplat_common.dart
+++ lib/src/common/xplat_common.dart
@@ -1,10 +1,10 @@
 import 'package:github/src/common.dart';
 
 /// Looks for GitHub Authentication information from the current environment.
-/// 
+///
 /// If in a browser context, it will look through query string parameters first
-/// and then sessionStorage. 
-/// 
+/// and then sessionStorage.
+///
 /// If in a server, command line or Flutter context it will use the system environment variables.
 ///
 /// In both contexts it delegates to [findAuthenticationInMap] to find the
@@ -12,7 +12,7 @@ import 'package:github/src/common.dart';
 Authentication findAuthenticationFromEnvironment() =>
     Authentication.anonymous();
 
-/// Checks the passed in map for keys in [COMMON_GITHUB_TOKEN_ENV_KEYS]. 
+/// Checks the passed in map for keys in [COMMON_GITHUB_TOKEN_ENV_KEYS].
 /// The first one that exists is used as the github token to call [Authentication.withToken] with.
 /// If the above fails, the GITHUB_USERNAME and GITHUB_PASSWORD keys will be checked.
 /// If those keys both exist, then [Authentication.basic] will be used.
