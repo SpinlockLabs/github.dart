c72b46031fcd326820cbc5bb0c3b4b1c15e075e4
Rob Becker <rob.becker@workiva.com>
2021-10-26 15:27:40 -0600

    update examples

---
+++ example/pr.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:github/github.dart';
+
+import 'common.dart';
+
+Future<void> main() async {
+  await initViewSourceButton('pr.dart');
+  var pr = await github.pullRequests
+      .get(RepositorySlug('flutter', 'flutter'), 90295);
+  renderPr(pr);
+}
+
+void renderPr(PullRequest pr) {
+  var prDiv = querySelector('#pr')!;
+  prDiv.innerText = pr.toJson().toString();
+}
