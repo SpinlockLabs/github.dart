b65b9c64b740f1cbbb2ce161dd5b30e8f30c9151
LaksCastro <lakscastro@gmail.com>
2021-11-11 06:16:40 -0300

    (#218) Add `Feed` model to handle Github Feeds API

--- lib/src/common/model/activity.dart
+++ lib/src/common/model/activity.dart
@@ -50,3 +50,102 @@ class RepositorySubscription {
       _$RepositorySubscriptionFromJson(input);
   Map<String, dynamic> toJson() => _$RepositorySubscriptionToJson(this);
 }
+
+@JsonSerializable()
+class Feed {
+  Feed({
+    this.timelineUrl,
+    this.userUrl,
+    this.currentUserPublicUrl,
+    this.currentUserUrl,
+    this.currentUserActorUrl,
+    this.currentUserOrganizationUrl,
+    this.currentUserOrganizationUrls,
+    this.securityAdvisoriesUrl,
+    this.links,
+  });
+
+  @JsonKey(name: 'timeline_url')
+  String? timelineUrl;
+
+  @JsonKey(name: 'user_url')
+  String? userUrl;
+
+  @JsonKey(name: 'current_user_public_url')
+  String? currentUserPublicUrl;
+
+  @JsonKey(name: 'current_user_url')
+  String? currentUserUrl;
+
+  @JsonKey(name: 'current_user_actor_url')
+  String? currentUserActorUrl;
+
+  @JsonKey(name: 'current_user_organization_url')
+  String? currentUserOrganizationUrl;
+
+  @JsonKey(name: 'current_user_organization_urls')
+  List<String>? currentUserOrganizationUrls;
+
+  @JsonKey(name: 'security_advisories_url')
+  String? securityAdvisoriesUrl;
+
+  @JsonKey(name: '_links')
+  FeedLinks? links;
+
+  factory Feed.fromJson(Map<String, dynamic> input) => _$FeedFromJson(input);
+  Map<String, dynamic> toJson() => _$FeedToJson(this);
+}
+
+@JsonSerializable()
+class FeedLinks {
+  FeedLinks({
+    this.timeline,
+    this.user,
+    this.currentUserPublic,
+    this.currentUser,
+    this.currentUserActor,
+    this.currentUserOrganization,
+    this.currentUserOrganizations,
+    this.securityAdvisories,
+  });
+
+  @JsonKey(name: 'timeline')
+  FeedLink? timeline;
+
+  @JsonKey(name: 'user')
+  FeedLink? user;
+
+  @JsonKey(name: 'current_user_public')
+  FeedLink? currentUserPublic;
+
+  @JsonKey(name: 'current_user')
+  FeedLink? currentUser;
+
+  @JsonKey(name: 'current_user_actor')
+  FeedLink? currentUserActor;
+
+  @JsonKey(name: 'current_user_organization')
+  FeedLink? currentUserOrganization;
+
+  @JsonKey(name: 'current_user_organizations')
+  List<FeedLink>? currentUserOrganizations;
+
+  @JsonKey(name: 'security_advisories')
+  FeedLink? securityAdvisories;
+
+  factory FeedLinks.fromJson(Map<String, dynamic> input) =>
+      _$FeedLinksFromJson(input);
+  Map<String, dynamic> toJson() => _$FeedLinksToJson(this);
+}
+
+@JsonSerializable()
+class FeedLink {
+  String? href;
+  String? type;
+
+  FeedLink(this.href, this.type);
+
+  factory FeedLink.fromJson(Map<String, dynamic> input) =>
+      _$FeedLinkFromJson(input);
+  Map<String, dynamic> toJson() => _$FeedLinkToJson(this);
+}
--- lib/src/common/model/activity.g.dart
+++ lib/src/common/model/activity.g.dart
@@ -53,3 +53,86 @@ Map<String, dynamic> _$RepositorySubscriptionToJson(
       'reason': instance.reason,
       'created_at': instance.createdAt?.toIso8601String(),
     };
+
+Feed _$FeedFromJson(Map<String, dynamic> json) => Feed(
+      timelineUrl: json['timeline_url'] as String?,
+      userUrl: json['user_url'] as String?,
+      currentUserPublicUrl: json['current_user_public_url'] as String?,
+      currentUserUrl: json['current_user_url'] as String?,
+      currentUserActorUrl: json['current_user_actor_url'] as String?,
+      currentUserOrganizationUrl:
+          json['current_user_organization_url'] as String?,
+      currentUserOrganizationUrls:
+          (json['current_user_organization_urls'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList(),
+      securityAdvisoriesUrl: json['security_advisories_url'] as String?,
+      links: json['_links'] == null
+          ? null
+          : FeedLinks.fromJson(json['_links'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$FeedToJson(Feed instance) => <String, dynamic>{
+      'timeline_url': instance.timelineUrl,
+      'user_url': instance.userUrl,
+      'current_user_public_url': instance.currentUserPublicUrl,
+      'current_user_url': instance.currentUserUrl,
+      'current_user_actor_url': instance.currentUserActorUrl,
+      'current_user_organization_url': instance.currentUserOrganizationUrl,
+      'current_user_organization_urls': instance.currentUserOrganizationUrls,
+      'security_advisories_url': instance.securityAdvisoriesUrl,
+      '_links': instance.links,
+    };
+
+FeedLinks _$FeedLinksFromJson(Map<String, dynamic> json) => FeedLinks(
+      timeline: json['timeline'] == null
+          ? null
+          : FeedLink.fromJson(json['timeline'] as Map<String, dynamic>),
+      user: json['user'] == null
+          ? null
+          : FeedLink.fromJson(json['user'] as Map<String, dynamic>),
+      currentUserPublic: json['current_user_public'] == null
+          ? null
+          : FeedLink.fromJson(
+              json['current_user_public'] as Map<String, dynamic>),
+      currentUser: json['current_user'] == null
+          ? null
+          : FeedLink.fromJson(json['current_user'] as Map<String, dynamic>),
+      currentUserActor: json['current_user_actor'] == null
+          ? null
+          : FeedLink.fromJson(
+              json['current_user_actor'] as Map<String, dynamic>),
+      currentUserOrganization: json['current_user_organization'] == null
+          ? null
+          : FeedLink.fromJson(
+              json['current_user_organization'] as Map<String, dynamic>),
+      currentUserOrganizations:
+          (json['current_user_organizations'] as List<dynamic>?)
+              ?.map((e) => FeedLink.fromJson(e as Map<String, dynamic>))
+              .toList(),
+      securityAdvisories: json['security_advisories'] == null
+          ? null
+          : FeedLink.fromJson(
+              json['security_advisories'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$FeedLinksToJson(FeedLinks instance) => <String, dynamic>{
+      'timeline': instance.timeline,
+      'user': instance.user,
+      'current_user_public': instance.currentUserPublic,
+      'current_user': instance.currentUser,
+      'current_user_actor': instance.currentUserActor,
+      'current_user_organization': instance.currentUserOrganization,
+      'current_user_organizations': instance.currentUserOrganizations,
+      'security_advisories': instance.securityAdvisories,
+    };
+
+FeedLink _$FeedLinkFromJson(Map<String, dynamic> json) => FeedLink(
+      json['href'] as String?,
+      json['type'] as String?,
+    );
+
+Map<String, dynamic> _$FeedLinkToJson(FeedLink instance) => <String, dynamic>{
+      'href': instance.href,
+      'type': instance.type,
+    };
