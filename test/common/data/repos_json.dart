4986ac130fd006bfaabe57c0ba1887ccd16628ec
Casey Hillers <chillers@google.com>
2023-05-11 09:21:12 -0700

    Add optional filter params on Repositories.listCommits (#368)
    * Add optional filter params on Repositories.listCommits
    
    * dart format
    
    ---------
    
    Co-authored-by: Rob Becker <rob.becker@workiva.com>

---
+++ test/common/data/repos_json.dart
@@ -0,0 +1,81 @@
+const String listCommits = '''
+[
+  {
+    "sha": "3771b3c9ab1912d32a0d0baffaf489d561caf558",
+    "node_id": "C_kwDOAVT0d9oAKDM3NzFiM2M5YWIxOTEyZDMyYTBkMGJhZmZhZjQ4OWQ1NjFjYWY1NTg",
+    "commit": {
+      "author": {
+        "name": "Rob Becker",
+        "email": "rob.becker@workiva.com",
+        "date": "2023-04-17T14:55:14Z"
+      },
+      "committer": {
+        "name": "Rob Becker",
+        "email": "rob.becker@workiva.com",
+        "date": "2023-04-17T14:55:14Z"
+      },
+      "message": "prep 9.12.0",
+      "tree": {
+        "sha": "282532b41e8fead81ec6d68e7e603139e7dd7581",
+        "url": "https://api.github.com/repos/SpinlockLabs/github.dart/git/trees/282532b41e8fead81ec6d68e7e603139e7dd7581"
+      },
+      "url": "https://api.github.com/repos/SpinlockLabs/github.dart/git/commits/3771b3c9ab1912d32a0d0baffaf489d561caf558",
+      "comment_count": 0,
+      "verification": {
+        "verified": true,
+        "reason": "valid"
+      }
+    },
+    "url": "https://api.github.com/repos/SpinlockLabs/github.dart/commits/3771b3c9ab1912d32a0d0baffaf489d561caf558",
+    "html_url": "https://github.com/SpinlockLabs/github.dart/commit/3771b3c9ab1912d32a0d0baffaf489d561caf558",
+    "comments_url": "https://api.github.com/repos/SpinlockLabs/github.dart/commits/3771b3c9ab1912d32a0d0baffaf489d561caf558/comments",
+    "author": {
+      "login": "robbecker-wf",
+      "id": 6053699,
+      "node_id": "MDQ6VXNlcjYwNTM2OTk=",
+      "avatar_url": "https://avatars.githubusercontent.com/u/6053699?v=4",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/robbecker-wf",
+      "html_url": "https://github.com/robbecker-wf",
+      "followers_url": "https://api.github.com/users/robbecker-wf/followers",
+      "following_url": "https://api.github.com/users/robbecker-wf/following{/other_user}",
+      "gists_url": "https://api.github.com/users/robbecker-wf/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/robbecker-wf/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/robbecker-wf/subscriptions",
+      "organizations_url": "https://api.github.com/users/robbecker-wf/orgs",
+      "repos_url": "https://api.github.com/users/robbecker-wf/repos",
+      "events_url": "https://api.github.com/users/robbecker-wf/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/robbecker-wf/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "committer": {
+      "login": "robbecker-wf",
+      "id": 6053699,
+      "node_id": "MDQ6VXNlcjYwNTM2OTk=",
+      "avatar_url": "https://avatars.githubusercontent.com/u/6053699?v=4",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/robbecker-wf",
+      "html_url": "https://github.com/robbecker-wf",
+      "followers_url": "https://api.github.com/users/robbecker-wf/followers",
+      "following_url": "https://api.github.com/users/robbecker-wf/following{/other_user}",
+      "gists_url": "https://api.github.com/users/robbecker-wf/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/robbecker-wf/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/robbecker-wf/subscriptions",
+      "organizations_url": "https://api.github.com/users/robbecker-wf/orgs",
+      "repos_url": "https://api.github.com/users/robbecker-wf/repos",
+      "events_url": "https://api.github.com/users/robbecker-wf/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/robbecker-wf/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "parents": [
+      {
+        "sha": "a3081681da68383d9a5f18ef3502f47f7144e7d8",
+        "url": "https://api.github.com/repos/SpinlockLabs/github.dart/commits/a3081681da68383d9a5f18ef3502f47f7144e7d8",
+        "html_url": "https://github.com/SpinlockLabs/github.dart/commit/a3081681da68383d9a5f18ef3502f47f7144e7d8"
+      }
+    ]
+  }
+]
+''';
