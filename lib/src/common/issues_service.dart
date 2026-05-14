329d852ac38ed58fd5acd516aaab2d67e9e2d5e7
Parker Lougheed <parlough@gmail.com>
2026-05-13 21:15:22 -0500

    Update IssuesService.updateComment to use PATCH instead of POST

--- lib/src/common/issues_service.dart
+++ lib/src/common/issues_service.dart
@@ -252,7 +252,7 @@ class IssuesService extends Service {
   /// API docs: https://docs.github.com/en/rest/reference/issues#update-an-issue-comment
   Future<IssueComment> updateComment(RepositorySlug slug, int id, String body) {
     final it = GitHubJson.encode({'body': body});
-    return github.postJSON(
+    return github.patchJSON(
       '/repos/${slug.fullName}/issues/comments/$id',
       body: it,
       convert: IssueComment.fromJson,
